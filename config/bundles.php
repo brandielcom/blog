4fa2b7a338241a6eea6b723b5269af27130e902c
Daniel Yang <hello@brandiel.com>
2020-05-28 00:05:20 +1200

    Install sonata admin and sonata doctrine orm admin bundle
    Add the followin two packages to composer.json and re-install
    packages removing vendor folder and composer.lock file.
    
    As "composer req bundle-name" doesn't work due to dependency
    problems.
    
    "sonata-project/admin-bundle": "^3.66",
    "sonata-project/doctrine-orm-admin-bundle": "^3.17"

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,9 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Sonata\CoreBundle\SonataCoreBundle::class => ['all' => true],
+    Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
+    Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
+    Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
 ];
